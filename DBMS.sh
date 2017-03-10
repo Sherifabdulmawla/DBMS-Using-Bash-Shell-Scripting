09b64a739ad3f245df48193f951e77dddd85a675
IslamWahid <eng.islamwahid@gmail.com>
2017-03-10 03:24:20 +0200

    Create Table edited

--- DBMS.sh
+++ DBMS.sh
@@ -2,7 +2,7 @@ mkdir DBMS 2>> ./error.log
 clear
 echo " -- Welcome To OUR DBMS --"
 function mainMenu() {
-  echo -e "\n\n+---------Main Menu-------------+"
+  echo -e "\n+---------Main Menu-------------+"
   echo "| 1. Select DB                  |"
   echo "| 2. Create DB                  |"
   echo "| 3. Rename DB                  |"
@@ -28,11 +28,11 @@ function selectDB {
   read dbName
   cd ./DBMS/$dbName 2>>./error.log
   if [[ $? == 0 ]]; then
-    echo "$dbName was Successfully Selected"
+    echo "Database $dbName was Successfully Selected"
     tablesMenu
   else
     echo "Database $dbName wasn't found"
-      mainMenu
+    mainMenu
 
   fi
 }
@@ -77,7 +77,7 @@ function dropDB {
 }
 
 function tablesMenu {
-  echo -e "\n\n+--------Tables Menu------------+"
+  echo -e "\n+--------Tables Menu------------+"
   echo "| 1. Create New Table           |"
   echo "| 2. Insert Into Table          |"
   echo "| 3. Select From Table          |"
@@ -91,11 +91,11 @@ function tablesMenu {
   read ch
   case $ch in
     1)  createTable ;;
-    2)  ;;
+    2)  insert;;
     3)  clear; selectMenu ;;
     4)  ;;
     5)  ;;
-    6)  dropTable ;;
+    6)  dropTable;;
     7) clear; mainMenu ;;
     8) exit ;;
     *) echo " Wrong Choice " ; mainMenu;
@@ -104,24 +104,61 @@ function tablesMenu {
 }
 
 function createTable {
-  echo -e "Enter Table Name \c"
+  echo -e "Table Name: \c"
   read tableName
-  echo -e "Enter Number of Columns \c"
+  if [[ -f $tableName ]]; then
+    echo "table already existed ,choose another name"
+    tablesMenu
+  fi
+  echo -e "Number of Columns: \c"
   read colsNum
-  re='^[0-9]+$'
-  if [[ $colsNum =~ $re ]]
-  then
-    counter=1
-    seperator="|"
-    while [ $counter -le $colsNum ]
-    do
-    echo -e "Enter Name of Column Number $counter:\c"
+  counter=1
+  sep="|"
+  rSep="\n"
+  pKey=""
+  metaData="Field"$sep"Type"$sep"key"
+  while [ $counter -le $colsNum ]
+  do
+    echo -e "Name of Column No.$counter: \c"
     read colName
-    temp=$temp$colName$seperator
-    ((counter++))
+
+    echo -e "Type of Column $colName: "
+    select var in "int" "str"
+    do
+      case $var in
+        int ) colType="int";break;;
+        str ) colType="str";break;;
+        * ) echo "Wrong Choice" ;;
+      esac
     done
-    touch $tableName 2>>./error.log
-    echo -e $temp >> $tableName
+    if [[ $pKey == "" ]]; then
+      echo -e "Make PrimaryKey ? "
+      select var in "yes" "no"
+      do
+        case $var in
+          yes ) pKey="PK";
+          metaData+=$rSep$colName$sep$colType$sep$pKey;
+          break;;
+          no )
+          metaData+=$rSep$colName$sep$colType$sep""
+          break;;
+          * ) echo "Wrong Choice" ;;
+        esac
+      done
+    else
+      metaData+=$rSep$colName$sep$colType$sep""
+    fi
+    if [[ $counter == $colsNum ]]; then
+      temp=$temp$colName
+    else
+      temp=$temp$colName$sep
+    fi
+    ((counter++))
+  done
+  touch .$tableName
+  echo -e $metaData  >> .$tableName
+  touch $tableName
+  echo -e $temp >> $tableName
     if [[ $? == 0 ]]
     then
       echo "Table Created Successfully"
@@ -150,37 +187,26 @@ function dropTable {
   tablesMenu
 }
 
-function selectMenu {
-  echo -e "\n\n+---------------Select Menu--------------------+"
-  echo "| 1. Select All Columns of a Table             |"
-  echo "| 2. Select Specific Column from a Table       |"
-  echo "| 3. Select From Table under condition         |"
-  echo "| 4. Aggregate Function for a Specific Column  |"
-  echo "| 5. Back To Main Menu                         |"
-  echo "| 6. Exit                                      |"
-  echo "+----------------------------------------------+"
-  echo -e "Enter Choice: \c"
-  read ch
-  case $ch in
-    1)  selectAll ;;
-    2)  ;;
-    3)  ;;
-    4)  ;;
-    5) clear; mainMenu ;;
-    6) exit ;;
-    *) echo " Wrong Choice " ; mainMenu;
-  esac
-}
-
-function selectAll {
-  echo -e "Enter Table Name: \c"
-  read tName
-  column -t -s '|' $tName 2>>./error.log
-  if [[ $? != 0 ]]
-  then
-    echo "Error Displaying Table $tName"
+function insert {
+  echo -e "Table Name: \c"
+  read tableName
+  if ! [[ -f $tableName ]]; then
+    echo "Table $tableName isn't existed ,choose another Table"
+    tablesMenu
   fi
-  selectMenu
+  for colName in `awk ' BEGIN{FS="|" ; ORS=" "} {if(NR!=1)print $1} ' $tableName`
+  do
+    echo "$colName"
+  done
 }
 
+function dropTable {
+  echo -e "Table Name: \c"
+  read tableName
+  if ! [[ -f $tableName ]]; then
+    echo "Table $tableName isn't existed ,choose another Table"
+    tablesMenu
+  fi
+  rm $tablesName .$tableName
+}
 mainMenu
