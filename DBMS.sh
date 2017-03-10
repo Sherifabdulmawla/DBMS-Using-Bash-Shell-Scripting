9d2d2d65796acecf1bfdbb757f7747cb7b673497
Sherifabdulmawla <sherifabdulmawla@gmail.com>
2017-03-10 08:38:17 +0200

    final selection from tables with conditions with error handling

--- DBMS.sh
+++ DBMS.sh
@@ -90,16 +90,16 @@ function tablesMenu {
   echo -e "Enter Choice: \c"
   read ch
   case $ch in
-    1)  ls .;tablesMenu ;;
+    1)  ls .; tablesMenu ;;
     2)  createTable ;;
     3)  insert;;
     4)  clear; selectMenu ;;
     5)  ;;
     6)  ;;
     7)  dropTable;;
-    8) clear; mainMenu ;;
+    8) clear; cd ../.. 2>>./.error.log; mainMenu ;;
     9) exit ;;
-    *) echo " Wrong Choice " ; mainMenu;
+    *) echo " Wrong Choice " ; tablesMenu;
   esac
 
 }
@@ -125,7 +125,7 @@ function createTable {
 
     echo -e "Type of Column $colName: "
     select var in "int" "str"
-      do
+    do
       case $var in
         int ) colType="int";break;;
         str ) colType="str";break;;
@@ -217,23 +217,25 @@ function insert {
 
 function selectMenu {
   echo -e "\n\n+---------------Select Menu--------------------+"
-  echo "| 1. Select All Columns of a Table             |"
-  echo "| 2. Select Specific Column from a Table       |"
-  echo "| 3. Select From Table under condition         |"
-  echo "| 4. Aggregate Function for a Specific Column  |"
-  echo "| 5. Back To Main Menu                         |"
-  echo "| 6. Exit                                      |"
+  echo "| 1. Select All Columns of a Table              |"
+  echo "| 2. Select Specific Column from a Table        |"
+  echo "| 3. Select From Table under condition          |"
+  echo "| 4. Aggregate Function for a Specific Column   |"
+  echo "| 5. Back To Tables Menu                        |"
+  echo "| 6. Back To Main Menu                          |"
+  echo "| 7. Exit                                       |"
   echo "+----------------------------------------------+"
   echo -e "Enter Choice: \c"
   read ch
   case $ch in
-    1)  selectAll ;;
-    2)  selectCol ;;
-    3)  ;;
-    4)  ;;
-    5) clear; mainMenu ;;
-    6) exit ;;
-    *) echo " Wrong Choice " ; mainMenu;
+    1) selectAll ;;
+    2) selectCol ;;
+    3) clear; selectCon ;;
+    4) ;;
+    5) clear; tablesMenu ;;
+    6) clear; cd ../.. 2>>./.error.log; mainMenu ;;
+    7) exit ;;
+    *) echo " Wrong Choice " ; selectMenu;
   esac
 }
 
@@ -253,7 +255,96 @@ function selectCol {
   read tName
   echo -e "Enter Column Number: \c"
   read colNum
-  awk 'BEGIN{FS="|"}{print $'$colNum'}' $tName
+  awk 'BEGIN{FS="|"}{print $'$colNum'}' $tName 
   selectMenu
 }
+
+function selectCon {
+  echo -e "\n\n+--------Select Under Condition Menu-----------+"
+  echo "| 1. Select All Columns Matching Condition    |"
+  echo "| 2. Select Specific Column Matching Condition|"
+  echo "| 3. Back To Selection Menu                   |"
+  echo "| 4. Back To Main Menu                        |"
+  echo "| 5. Exit                                     |"
+  echo "+---------------------------------------------+"
+  echo -e "Enter Choice: \c"
+  read ch
+  case $ch in
+    1) clear; allCond ;;
+    2) clear; specCond ;;
+    3) clear; selectCon ;;
+    4) clear; cd ../.. 2>>./.error.log; mainMenu ;;
+    5) exit ;;
+    *) echo " Wrong Choice " ; selectCon;
+  esac
+}
+
+function allCond {
+  echo -e "Select all columns from TABLE Where FIELD(OPERATOR)VALUE \n"
+  echo -e "Enter Table Name: \c"
+  read tName
+  echo -e "Enter required FIELD name: \c"
+  read field
+  fid=$(awk 'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$field'") print i}}}' $tName)
+  if [[ $fid == "" ]]
+  then
+    echo "Not Found"
+    selectCon
+  else
+    echo -e "\nSupported Operators: [==, !=, >, <, >=, <=] \nSelect OPERATOR: \c"
+    read op
+    if [[ $op == "==" ]] || [[ $op == "!=" ]] || [[ $op == ">" ]] || [[ $op == "<" ]] || [[ $op == ">=" ]] || [[ $op == "<=" ]]
+    then
+      echo -e "\nEnter required VALUE: \c"
+      read val
+      res=$(awk 'BEGIN{FS="|"}{if ($'$fid$op$val') print $0}' $tName 2>>./.error.log |  column -t -s '|') 
+      if [[ $res == "" ]]
+      then
+        echo "Value Not Found"
+        selectCon
+      else
+        awk 'BEGIN{FS="|"}{if ($'$fid$op$val') print $0}' $tName 2>>./.error.log |  column -t -s '|' 
+        selectCon
+      fi
+    else
+      echo "Unsupported Operator\n"
+      selectCon
+    fi
+  fi
+}
+
+function specCond {
+  echo -e "Select specific column from TABLE Where FIELD(OPERATOR)VALUE \n"
+  echo -e "Enter Table Name: \c"
+  read tName
+  echo -e "Enter required FIELD name: \c"
+  read field
+  fid=$(awk 'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$field'") print i}}}' $tName)
+  if [[ $fid == "" ]]
+  then
+    echo "Not Found"
+    selectCon
+  else
+    echo -e "\nSupported Operators: [==, !=, >, <, >=, <=] \nSelect OPERATOR: \c"
+    read op
+    if [[ $op == "==" ]] || [[ $op == "!=" ]] || [[ $op == ">" ]] || [[ $op == "<" ]] || [[ $op == ">=" ]] || [[ $op == "<=" ]]
+    then
+      echo -e "\nEnter required VALUE: \c"
+      read val
+      res=$(awk 'BEGIN{FS="|"; ORS="\n"}{if ($'$fid$op$val') print $'$fid'}' $tName 2>>./.error.log |  column -t -s '|') 
+      if [[ $res == "" ]]
+      then
+        echo "Value Not Found"
+        selectCon
+      else
+        awk 'BEGIN{FS="|"; ORS="\n"}{if ($'$fid$op$val') print $'$fid'}' $tName 2>>./.error.log |  column -t -s '|'
+        selectCon
+      fi
+    else
+      echo "Unsupported Operator\n"
+      selectCon
+    fi
+  fi
+}
+
 mainMenu
